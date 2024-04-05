da21cec3a67dd1e9400cf206849d20d32fbfe8df
Marco <marcomessina90121@gmail.com>
2024-04-05 14:45:49 +0300

    Created decode function

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
 import pandas as pd
 import re
+data = pd.read_csv('morse_base.csv', on_bad_lines='skip')
+def check():
+    global text_x
+    text_x = input("\nGive here the Morse code or plain text: ").upper()
+    if re.match("[0-9A-Z]+", text_x):
+        print("True1 Plain Text\n")
+        pass
+    elif re.match("[ .,+∙•●◎☉⦾⦿◍◌○◉⊛⊚⊙◦。❍⬤⚉⚇◑◐◓◒◔◕◙◘〇◯⊖⊕⚯⧂⚫_-]", text_x):
+        print("That's Morse")
+        decode()
+    else:
+        print("There are not enough symbols. Try again.")
+        check()
+
+def decode():
+    split_the_input= text_x.split(' ')
+    full_decode_text = ""
+    for d in split_the_input:
+        matches = data[data['code'] == d]
+        if not matches.empty:
+            equal_letter = matches.iloc[0]['symbol']
+            print(f"Equal for combination '{d}': {equal_letter}")
+            full_decode_text += str(equal_letter)
+        else:
+            print(f"No match found for combination '{d}'")
+            full_decode_text += str(" ")
+    print(f"\n{full_decode_text}")
+    check()
+check()
