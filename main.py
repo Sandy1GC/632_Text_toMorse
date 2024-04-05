fca9cfa6d814a7e98ee0a7d29b4a02474a39b937
Marco <marcomessina90121@gmail.com>
2024-04-05 14:49:56 +0300

    Created encode function. ok

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def check():
     text_x = input("\nGive here the Morse code or plain text: ").upper()
     if re.match("[0-9A-Z]+", text_x):
         print("True1 Plain Text\n")
-        pass
+        encode()
     elif re.match("[ .,+∙•●◎☉⦾⦿◍◌○◉⊛⊚⊙◦。❍⬤⚉⚇◑◐◓◒◔◕◙◘〇◯⊖⊕⚯⧂⚫_-]", text_x):
         print("That's Morse")
         decode()
@@ -27,5 +27,20 @@ def decode():
             print(f"No match found for combination '{d}'")
             full_decode_text += str(" ")
     print(f"\n{full_decode_text}")
+    check()
+
+def encode():
+    encode_the_input = list(text_x)
+    full_code_text = ""
+    for e in encode_the_input:
+        matches = data[data['symbol'] == e]
+        if not matches.empty:
+            code_letter = matches.iloc[0]['code']
+            print(f"Encode symbol'{e}': [{code_letter}]")
+            full_code_text += str(f' {code_letter}')
+        else:
+            print(f"No match found for symbol '{e}'")
+            full_code_text += str(" ")
+    print(f"\n{full_code_text}")
     check()
 check()
